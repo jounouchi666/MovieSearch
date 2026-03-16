3634793ea2d25bde892c3dc0883778f5a05e1ef9
jounouchi666 <trueguy666@gmail.com>
2026-03-16 17:57:20 +0900

    JsonResource作成

---
+++ backend/app/Http/Resources/MovieResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MovieResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'page' => $this->page,
+            'results' => MovieItemResource::collection($this->results),
+            'total_pages' => $this->totalPages,
+            'total_results' => $this->totalResults
+        ];
+    }
+}
