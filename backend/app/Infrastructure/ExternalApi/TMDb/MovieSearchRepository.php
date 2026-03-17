bc5b5468ab5d83eb458fd3587d1bac6dc4cdbe74
jounouchi666 <trueguy666@gmail.com>
2026-03-17 18:14:10 +0900

    責務の分離

--- backend/app/Infrastructure/ExternalApi/TMDb/MovieSearchRepository.php
+++ backend/app/Infrastructure/ExternalApi/TMDb/MovieSearchRepository.php
@@ -5,11 +5,9 @@ namespace App\Infrastructure\ExternalApi\TMDb;
 use App\Application\Query\MovieSearchQuery;
 use App\Application\DTO\MovieSearchResultDTO;
 use App\Application\Repository\MovieSearchRepositoryInterface;
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use RuntimeException;
+use App\Infrastructure\ExternalApi\TMDb\Service\MovieGenreCacheService;
+use App\Infrastructure\ExternalApi\TMDb\Service\TMDbMovieSearchMapper;
+use App\Infrastructure\ExternalApi\TMDb\Service\TMDbRequestExecutor;
 
 /**
  * TMDbを使用した映画検索の実装
@@ -17,7 +15,9 @@ use RuntimeException;
 class MovieSearchRepository implements MovieSearchRepositoryInterface
 {
     public function __construct(
-        private MovieSearchAssembler $movieSearchAssembler
+        private TMDbMovieSearchMapper $TMDbMovieSearchMapper,
+        private MovieGenreCacheService $movieGenreCacheService,
+        private TMDbRequestExecutor $tmdbRequestExecutor
     ) {}
 
     /**
@@ -28,58 +28,18 @@ class MovieSearchRepository implements MovieSearchRepositoryInterface
      */
     public function search(MovieSearchQuery $query): MovieSearchResultDTO
     {
-        $uri = 'https://api.themoviedb.org/3/search/movie?query=X-MEN&include_adult=true&language=ja-JP&page=1';
-
-        return $this->movieSearchAssembler->toDTO(
-            $this->executeRequest($uri, $query->toCleanArray()),
-            $this->getGenreMap()
+        return $this->TMDbMovieSearchMapper->toDTO(
+            $this->tmdbRequestExecutor->executeRequest(
+                config('tmdb.search_url'),
+                [
+                    'query' => $query->title,
+                    'include_adult' => $query->includeAdult,
+                    'year' => $query->year,
+                    'page' => $query->page,
+                    'language' => config('tmdb.language')
+                ]
+            ),
+            $this->movieGenreCacheService->getGenreMap()
         );
-    }
-    
-    /**
-     * ジャンルマップを取得
-     * 24時間有効のキャッシュ
-     *
-     * @return array
-     */
-    private function getGenreMap(): array
-    {
-        return [];
-    }
-    
-    /**
-     * リクエスト実行
-     *
-     * @param  string $uri
-     * @return void
-     */
-    private function executeRequest(string $uri, array $query = []): Collection
-    {
-        try {
-            return Http::timeout(3)
-                ->withToken($this->getToken())
-                ->withHeaders(['accept' => 'application/json'])
-                ->get($uri, $query)
-                ->throw()
-                ->collect();
-        } catch (RequestException $e) {
-            Log::error("TMDb API Error: {$uri}", ['message' => $e->getMessage()]);
-            throw new RuntimeException('外部APIとの通信に失敗しました');
-        }
-    }
-    
-    /**
-     * APIトークンの取得
-     *
-     * @return string
-     */
-    private function getToken(): string
-    {
-        $token = env('TMDB_API_TOKEN');
-        if (is_null($token)) {
-            throw new RuntimeException('APIトークンが見つかりません');
-        }
-
-        return $token;
     }
 }
