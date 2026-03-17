f4354f5375166b7154f2472ed6e23709496441a0
jounouchi666 <trueguy666@gmail.com>
2026-03-17 18:04:09 +0900

    キー名変更

--- backend/app/Http/Resources/MovieGenreResource.php
+++ backend/app/Http/Resources/MovieGenreResource.php
@@ -16,7 +16,7 @@ class MovieGenreResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'title' => $this->title
+            'name' => $this->name
         ];
     }
 }
