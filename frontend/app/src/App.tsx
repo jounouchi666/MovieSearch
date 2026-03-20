d2f1ccac4c16a13a2e6952146214a7d45e746763
jounouchi666 <trueguy666@gmail.com>
2026-03-20 13:56:16 +0900

    ルート追加

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+import { Search } from "./pages/Search"
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Search />}/>
+    </Routes>
   )
 }
 
