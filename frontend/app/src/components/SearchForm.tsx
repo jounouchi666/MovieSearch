07f62b8b54ffc699332ee6ca4634b5ca5491a8ff
jounouchi666 <trueguy666@gmail.com>
2026-03-21 15:36:11 +0900

    入力値の保持、バリデーションエラーの配置

--- frontend/app/src/components/SearchForm.tsx
+++ frontend/app/src/components/SearchForm.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import type { ValidationError } from "../types/validationError";
+import ErrorMessage from "./ErrorMessage";
 
 type Props = {
     onSearch: (title: string, includeAdult: boolean, page: number) => void;
+    titleInUrl: string;
+    includeAdultInUrl: boolean;
     loading: boolean;
+    validationErrors: ValidationError['errors']|null;
 }
 
-const SearchForm = ({ onSearch, loading}: Props) => {
-    const [title, setTitle] = useState('');
-    const [includeAdult, setIncludeAdult] = useState(false);
+const SearchForm = ({ onSearch, titleInUrl, includeAdultInUrl, loading, validationErrors}: Props) => {
+    const [title, setTitle] = useState(titleInUrl);
+    const [includeAdult, setIncludeAdult] = useState(includeAdultInUrl);
 
     const submit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -19,31 +24,45 @@ const SearchForm = ({ onSearch, loading}: Props) => {
     return (
         <form className="mt-5 flex justify-center" onSubmit={submit}>
             <div className="max-w-full flex flex-col gap-2">
-                <div className="flex gap-3">
-                    <input
-                        className="w-md border-color-white outline-slate-300 shadow-sm rounded p-2"
-                        type="text"
-                        name="title"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
-                        placeholder="タイトルを入力"
-                    />
-                    <button
-                        className="shrink-0 border rounded shadow-sm text-white py-2 w-24 bg-cyan-500 transition-colors cursor-pointer hover:bg-cyan-600"
-                        type="submit"
-                        disabled={loading}
-                    >
-                        検索   
-                    </button>
+                <div className="flex flex-col gap-1">
+                    <div className="flex gap-3">
+                        <input
+                            className="w-md border-color-white outline-slate-300 shadow-sm rounded p-2"
+                            type="text"
+                            name="title"
+                            value={title}
+                            maxLength={255}
+                            onChange={e => setTitle(e.target.value)}
+                            placeholder="タイトルを入力"
+                        />
+                        <button
+                            className="shrink-0 border rounded shadow-sm text-white py-2 w-24 bg-cyan-500 transition-colors cursor-pointer hover:bg-cyan-600"
+                            type="submit"
+                            disabled={loading}
+                        >
+                            検索   
+                        </button>
+                    </div>
+                    <div>
+                        {validationErrors?.['title'] &&
+                            <ErrorMessage messages={validationErrors['title']}/>
+                        }
+                    </div>
                 </div>
-                <label className="w-full flex items-center gap-2 text-sm text-gray-600">
+                
+                <div className="flex flex-col gap-1">
+                    <label className="w-full flex items-center gap-2 text-sm text-gray-600">
                     <input
                         type="checkbox"
                         checked={includeAdult}
                         onChange={() => setIncludeAdult(!includeAdult)}
                     />
                         成人向け作品を含める
-                </label>
+                    </label>
+                    {validationErrors?.['include_adult'] &&
+                        <ErrorMessage messages={validationErrors['include_adult']}/>
+                    }
+                </div>
             </div>
         </form>
     );
