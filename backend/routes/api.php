ef0b6c385bc2bdbe451dca8de5acd9ece52491cb
jounouchi666 <trueguy666@gmail.com>
2026-03-23 16:54:30 +0900

    不要なルートを削除

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('v1')->group(function() {
     Route::get('search', [MovieController::class, 'search']);
 });
